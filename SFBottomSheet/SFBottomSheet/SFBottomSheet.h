8d6353cf29e4823d635671a06dbe110f41e66836
Aleksandar Gyuzelov <aleksandar.gyuzelov@scalefocus.com>
2021-02-16 22:20:20 +0200

    Initial project setup

---
+++ SFBottomSheet/SFBottomSheet/SFBottomSheet.h
@@ -0,0 +1,18 @@
+//
+//  SFBottomSheet.h
+//  SFBottomSheet
+//
+//  Created by Aleksandar Gyuzelov on 16.02.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SFBottomSheet.
+FOUNDATION_EXPORT double SFBottomSheetVersionNumber;
+
+//! Project version string for SFBottomSheet.
+FOUNDATION_EXPORT const unsigned char SFBottomSheetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SFBottomSheet/PublicHeader.h>
+
+
