3c7266950b930e29c3f5609f996874430291f1c7
Aleksandar Gyuzelov <aleksandar.gyuzelov@scalefocus.com>
2021-04-01 01:37:03 +0300

    Updating .travis.yml file

--- SFBottomSheet/SFBottomSheetDemoTests/SFBottomSheetDemoTests.swift
+++ SFBottomSheet/SFBottomSheetDemoTests/SFBottomSheetDemoTests.swift
@@ -23,11 +23,4 @@ class SFBottomSheetDemoTests: XCTestCase {
         XCTAssert(true)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
